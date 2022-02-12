b1b49fde96916bf2f0687ff3d5fe95d80bf80ec3
Alan Eduardo Tapia Vazquez <98989526+TaponTV@users.noreply.github.com>
2022-02-12 12:59:39 -0600

    Update Utilities.java

--- GenericTuple/src/main/java/uv/generictuple/Utilities.java
+++ GenericTuple/src/main/java/uv/generictuple/Utilities.java
@@ -3,7 +3,6 @@ package uv.generictuple;
 public class Utilities{
     
     public static <T extends GenericTupla> boolean compare (T point1, T point2){
-        if(!(point1 instanceof GenericTupla)||!(point2 instanceof GenericTupla)) return false;
         return point1.getKey().equals(point2.getKey()) && point1.getValue().equals(point2.getValue());
     }
     
