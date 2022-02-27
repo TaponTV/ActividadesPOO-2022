5c7bea0f4d16af893b930f542fc9aa5d81e30977
Alan Eduardo Tapia Vazquez <98989526+TaponTV@users.noreply.github.com>
2022-02-26 22:16:14 -0600

    Interface Basics

---
+++ InterfaceBasics/src/main/java/uv/interfacebasics/draw.java
@@ -0,0 +1,11 @@
+package uv.interfacebasics;
+public interface draw {
+    int RED = 1;
+    int GREEN = 1;
+    int BLUE = 3;
+    int BLACK = 4;
+    int WHITE = 5;
+    
+    void draw(int color);
+    void fill(int color);
+}
