28a00eab1a0346bdf9d6102d4d84374d5505a5ff
Alan Eduardo Tapia Vazquez <98989526+TaponTV@users.noreply.github.com>
2022-02-26 22:15:38 -0600

    Delete InterfaceBasics directory

--- InterfaceBasics/InterfaceBasics/src/main/java/uv/interfacebasics/calculate.java
+++
@@ -1,4 +0,0 @@
-package uv.interfacebasics;
-public interface calculate{
-    public double calculateArea();
-}
--- InterfaceBasics/src/main/java/uv/interfacebasics/EntryPoint.java
+++
@@ -1,6 +0,0 @@
-package uv.interfacebasics;
-public class EntryPoint {
-    public static void main (String []  args){
-
-    }
-}
